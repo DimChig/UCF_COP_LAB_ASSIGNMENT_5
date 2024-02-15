2f263932ad4c6082c104525bcb039d216593aa60
DimChig <dima.chigarev@gmail.com>
2024-02-15 14:53:10 -0500

    [my_working_branch] test

--- lab_assignment_5.c
+++ lab_assignment_5.c
@@ -2,22 +2,22 @@
 #include <stdlib.h>
 
 typedef struct node {
-	char letter;
-	struct node* next;
+    char letter;
+    struct node* next;
 } node;
 
 // Returns number of nodes in the linkedList.
 int length(node* head)
 {
-   struct node *tmp = head;
-    int len = 0;
-   while (tmp != NULL)
-   {
-      tmp = tmp->next;
-      len++;
-   }
-
-   return (len);
+    if (head == NULL) return 0; //check
+    node* temp = head;
+    int l = 0; //lengths counter
+    while (temp->next != NULL) { //loop until reaching the end (when temp will = lastNode)
+        temp = temp->next; //move to the next node
+        l++; //increment counter
+    }
+    l++; //last character
+    return l;
 }
 
 // parses the string in the linkedList
@@ -25,6 +25,18 @@ int length(node* head)
 //  then toCString function wil return "abc"
 char* toCString(node* head)
 {
+    if (head == NULL) return ""; //check
+    node* temp = head;
+    char * s = (char*) malloc(sizeof(char) * (length(head) + 1)); //allocating memory (+1 for '\0')
+    int i = 0; //counter
+    while (temp->next != NULL) { //loop
+        s[i] = temp->letter; //saving letter
+        i++; //increment
+        temp = temp->next; //moving to next node
+    }
+    s[i] = temp->letter; //saving last letter
+    s[i+1] = '\0'; //saving null character
+    return s; //returning pointer to the first letter
 }
 
 // inserts character to the linkedlist
@@ -33,44 +45,78 @@ char* toCString(node* head)
 // head -> |a|->|b|->|c|->|x|
 void insertChar(node** pHead, char c)
 {
+    if (pHead == NULL) return; //checker
+
+    //Creating new letter
+    node* newNode = (node*) malloc(sizeof (node));
+    if (newNode == NULL) {
+        printf("Failed to allocate memory!");
+        return;
+    }
+    //changing newNode values
+    newNode->letter = c;
+    newNode->next = NULL;
+
+    node* temp = *pHead;
+    if (temp == NULL) { //checking if we have other elements in the list
+        //if not, replacing pHead with our new Node
+        *pHead = newNode;
+        return;
+    }
+
+    //else, looping until the end of the list
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode; //updating ".next" of the last note to newNode
 }
 
 // deletes all nodes in the linkedList.
 void deleteList(node** pHead)
 {
+    if (pHead == NULL) return;
+    node* temp = *pHead;
+    node* nextNode;
+    while (temp->next != NULL) { //looping
+        nextNode = temp->next; //saving reference
+        free(temp); //freeing memory
+        temp = nextNode; //moving forward
+    }
+    free(nextNode); //freeing last node
+    *pHead = NULL; //set head to null
 }
 
 int main(void)
 {
-	int i, strLen, numInputs;
-	node* head = NULL;
-	char* str;
-	char c;
-	FILE* inFile = fopen("input.txt","r");
+    int i, strLen, numInputs;
+    node* head = NULL;
+    char* str;
+    char c;
+    FILE* inFile = fopen("input.txt","r");
 
-	fscanf(inFile, " %d\n", &numInputs);
-	
-	while (numInputs-- > 0)
-	{
-		fscanf(inFile, " %d\n", &strLen);
-		for (i = 0; i < strLen; i++)
-		{
-			fscanf(inFile," %c", &c);
-			insertChar(&head, c);
-		}
+    fscanf(inFile, " %d\n", &numInputs);
 
-		str = toCString(head);
-		printf("String is : %s\n", str);
+    while (numInputs-- > 0)
+    {
+        fscanf(inFile, " %d\n", &strLen);
+        for (i = 0; i < strLen; i++)
+        {
+            fscanf(inFile," %c", &c);
+            insertChar(&head, c);
+        }
 
-		free(str);
-		deleteList(&head);
+        str = toCString(head);
+        printf("String is : %s\n", str);
 
-		if (head != NULL)
-		{
-			printf("deleteList is not correct!");
-			break;
-		}
-	}
+        free(str);
+        deleteList(&head);
 
-	fclose(inFile);
+        if (head != NULL)
+        {
+            printf("deleteList is not correct!");
+            break;
+        }
+    }
+
+    fclose(inFile);
 }
